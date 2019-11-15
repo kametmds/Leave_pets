d70e58c49d073e45c04107f40ea3d7ad33af960a
shotaro yuki <yuki8118tmds@gmail.com>
2019-11-15 13:02:28 +0900

    login logout

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,6 @@
 class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @pets = Pet.all
