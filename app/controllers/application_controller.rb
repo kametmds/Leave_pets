12b509092802f7020634930cd9c7e2949319d4a3
shotaro yuki <yuki8118tmds@gmail.com>
2019-11-18 13:08:24 +0900

    devise user image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:name, :image, :image_cache]
+    added_attrs = [:name, :icon, :icon_cache]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
