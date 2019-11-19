d2aeca042fac56399a84aa0bf774e4fbb79b133a
shotaro yuki <yuki8118tmds@gmail.com>
2019-11-19 11:43:14 +0900

    css fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pets
   root 'pets#index'
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
