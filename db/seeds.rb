524c7866514059adda3d6ce5226bfe763e042bee
shotaro yuki <yuki8118tmds@gmail.com>
2019-11-21 14:28:11 +0900

    seedcreate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 1.upto(6) do |i|
+#   User.create!(name: "user#{i}", email: "user#{i}@test.com", password: "user#{i}@test.com")
+# end
+
+# User.create!(name: "Admin", email: "admin@test.com", password: "admin@test.com", admin: true)
+
+# 1.upto(3) do |i|
+#   Pet.create!(name: "cat#{i}", age: i, size: i, weight: i, content: i, image: open("#{Rails.root}/db/data/cat#{i}.jpg"), user_id: User.first.id)
+# end
+
+# 1.upto(2) do |i|
+#   Pet.create!(name: "dog#{i}", age: i, size: i, weight: i, content: i, image: open("#{Rails.root}/db/data/dog#{i}.jpg"), user_id: User.first.id + 1)
+# end
+
+1.upto(2) do |i|
+  Space.create!(title: "space#{i}", postal: "111-1111",address: "北海道",
+                tel: "07012345678", capacity: 2, picture:open("#{Rails.root}/db/data/window.jpg",
+                content: "#{i}番目の物件です", user: User.first))
+end
+# 3.upto(4) do |i|
+#   Space.create!(title: "space#{i}", postal: "222-2222",address: i ,
+#                 tel: "07087654321", capacity: i, picture:open("#{Rails.root}/db/data/window.jpg",
+#                 content: "#{i}番目の物件です", user_id: User.first.id + 1))
+# end
