b1cd8526fbbfb3b614e1326cfe0075f17594310b
shotaro yuki <yuki8118tmds@gmail.com>
2019-11-29 17:56:15 +0900

    label design create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
+
+  include UsersHelper
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to users_path
+    end
+  end
+
+  def user_check(user)
+    current_user.id == user.id
+  end
 end
