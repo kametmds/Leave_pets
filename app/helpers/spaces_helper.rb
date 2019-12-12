0842e3f85690deec1d804bc12c31d47a11ee39ed
shotaro yuki <yuki8118tmds@gmail.com>
2019-12-12 16:11:35 +0900

    space show css fix

--- app/helpers/spaces_helper.rb
+++ app/helpers/spaces_helper.rb
@@ -2,5 +2,19 @@ module SpacesHelper
   def rating_average(space)
     space.reviews.average(:rate).floor(2)
   end
+
   include ActsAsTaggableOn::TagsHelper
+
+  def format_phone(str)
+    return str if str.blank?
+    case str.size
+    when 10
+      str.gsub(/(\d{2})(\d{4})(\d{4})/, '\1-\2-\3')
+    when 11
+      str.gsub(/(\d{3})(\d{4})(\d{4})/, '\1-\2-\3')
+    else
+      str
+    end
+  end
+
 end
