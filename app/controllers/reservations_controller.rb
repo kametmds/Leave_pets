5d756cb012727bb0689c2c92ca477772a7b5ff2e
shotaro yuki <yuki8118tmds@gmail.com>
2019-12-14 17:58:27 +0900

    reservation createaction create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -39,7 +39,25 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
   end
 
+  # def start_date_join
+  #   date = params[:reservation][:start_date]
+  #   if date["start_date(1i)"].empty? && date["start_date(2i)"].empty? && date["start_date(3i)"].empty?
+  #     return
+  #   end
+  #   Date.new date["start_date(1i)"].to_i,date["start_date(2i)"].to_i,date["start_date(3i)"].to_i
+  # end
+
+  # def end_date_join
+  #   date = params[:reservation][:end_date]
+  #   if date["end_date(1i)"].empty? && date["end_date(2i)"].empty? && date["end_date(3i)"].empty?
+  #     return
+  #   end
+  #   Date.new date["end_date(1i)"].to_i,date["end_date(2i)"].to_i,date["end_date(3i)"].to_i
+  # end
+
   def reservation_params
+    # params[:reservation][:start_date ] = start_date_join
+    # params[:reservation][:end_date ] = end_date_join
     params.require(:reservation).permit(:end_date, :start_date, :status, :user_id, :space_id, :pet_id, pet_ids: [])
   end
 
