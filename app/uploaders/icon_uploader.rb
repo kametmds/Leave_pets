7d1b5c364e9a45dee7eac26a8746def111943d0b
shotaro yuki <yuki8118tmds@gmail.com>
2019-12-20 18:33:30 +0900

    heroku deploy

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -1,6 +1,10 @@
 class IconUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :file
+  if Rails.env.production? || Rails.env.staging?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
